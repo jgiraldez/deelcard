689b53fe54dfbed6c01317b128f8037eac536ab7
Juan Manuel <juanmanuel@gmail.com>
2025-12-29 17:40:10 -0300

    .

--- lib/env.ts
+++ lib/env.ts
@@ -5,10 +5,6 @@ export const env = {
   database: {
     url: process.env.DATABASE_URL,
   },
-  clerk: {
-    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    secretKey: process.env.CLERK_SECRET_KEY,
-  },
   r2: {
     accountId: process.env.R2_ACCOUNT_ID,
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
